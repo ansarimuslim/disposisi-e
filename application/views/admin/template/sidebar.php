228d193e6d195ea0bab3ec70f6bba1932d4d4b45
ansarimuslim <muhammadansarimuslim@gmail.com>
2024-01-10 21:38:59 +0800

    First Commit

---
+++ application/views/admin/template/sidebar.php
@@ -0,0 +1,35 @@
+<div id="layoutSidenav">
+    <div id="layoutSidenav_nav">
+        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+            <div class="sb-sidenav-menu">
+                <div class="nav">
+                    <div class="sb-sidenav-menu-heading">Core</div>
+                    <a class="nav-link" href="#">
+                        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                        Dashboard
+                    </a>
+                    <a class="nav-link" href="<?php echo site_url('admin/user'); ?>">
+                        <div class="sb-nav-link-icon"><i class="fas fa-user"></i></div>
+                        User
+                    </a>
+                    <a class="nav-link" href="<?php echo site_url('admin/surat_masuk'); ?>">
+                        <div class="sb-nav-link-icon"><i class="fas fa-envelope"></i></div>
+                        Surat Masuk
+                    </a>
+                    <a class="nav-link" href="<?php echo site_url('admin/surat_keluar'); ?>">
+                        <div class="sb-nav-link-icon"><i class="fas fa-envelope-open"></i></div>
+                        Surat Keluar
+                    </a>
+                    <a class="nav-link" href="<?php echo site_url('admin/disposisi'); ?>">
+                        <div class="sb-nav-link-icon"><i class="fas fa-square-check"></i></div>
+                        Disposisi
+                    </a>
+                </div>
+            </div>
+            <div class="sb-sidenav-footer">
+                <div class="small">Logged in as:</div>
+                Start Bootstrap
+            </div>
+        </nav>
+    </div>
+    <div id="layoutSidenav_content">
---
+++ db_disposisi.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 03, 2024 at 02:04 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_disposisi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `role` enum('admin','sekretaris') NOT NULL,
+  `last_login` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `is_active` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `username`, `password`, `email`, `full_name`, `phone`, `role`, `last_login`, `is_active`) VALUES
+(1, 'admin', '$2y$10$sSitIy9UrELame87IVZQFero9UebzWiQ88JEdYXcmwjeCn16r4Qdq', 'adithya@gmail.com', 'Adithya Suryanata Winardi', '089523236137', 'admin', '2024-01-03 13:03:11', 1),
+(2, 'Sya', '$2y$10$/s3gGNMdmoFG7v6bqdGsneKr9PMx3eGvQejEQ/1pGtZRuqZFbRwle', 'sya@gmail.com', 'Syaaa', '089537128123', 'sekretaris', '2024-01-03 13:04:35', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
